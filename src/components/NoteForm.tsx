291514a0d394f37fb6b03eb32305849bcd55871f
Ushan <ushanmithmakumara@outlook.com>
2023-08-24 14:57:21 +0530

    finalise project

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,31 +1,38 @@
-import { FormEvent, useRef, useState } from "react"
-import { Button, Col, Form, Row, Stack } from "react-bootstrap"
-import { Link, useNavigate } from "react-router-dom"
-import CreatableReactSelect from "react-select/creatable"
-import { NoteData, Tag } from "../App"
-import { v4 as uuidV4 } from 'uuid'
+import { FormEvent, useRef, useState } from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
+import { v4 as uuidV4 } from "uuid";
 
 type NoteFormProps = {
-  onSubmit: (data: NoteData) => void,
-  onAddTag: (tag: Tag) => void
-  availableTags: Tag[]
-}
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+} & Partial<NoteData>;
 
-function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
-  const titleRef = useRef<HTMLInputElement>(null)
-  const markdownRef = useRef<HTMLTextAreaElement>(null)
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
-  const navigate = useNavigate()
+function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
-    e.preventDefault()
+    e.preventDefault();
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: selectedTags
-    })
+      tags: selectedTags,
+    });
 
-    navigate('..')
+    navigate("..");
   }
 
   return (
@@ -35,42 +42,59 @@ function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
-              onCreateOption={label => {
-                const newTag = { label: label, id: uuidV4() }
-                onAddTag(newTag)
-                setSelectedTags(prev => [...prev, newTag])
-              }}
-              options={availableTags.map(tag => {
-                return { value: tag.id, label: tag.label }
-              })}
-              value={selectedTags.map(tag => {
-                return { label: tag.label, value: tag.id }
-              })} onChange={tags => setSelectedTags(tags.map(tag => {
-                return { label: tag.label, id: tag.value }
-              }))} isMulti />
+                onCreateOption={(label) => {
+                  const newTag = { label: label, id: uuidV4() };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                options={availableTags.map((tag) => {
+                  return { value: tag.id, label: tag.label };
+                })}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) =>
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  )
+                }
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
-          <Button type="submit" variant="primary">Save changes</Button>
+          <Button type="submit" variant="primary">
+            Save changes
+          </Button>
           <Link to="..">
-            <Button type="button" variant="outline-secondary">Cancel</Button>
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
           </Link>
         </Stack>
       </Stack>
     </Form>
-  )
+  );
 }
 
-export default NoteForm
+export default NoteForm;
